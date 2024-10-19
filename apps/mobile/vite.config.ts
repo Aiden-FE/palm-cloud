69c95f02d52c23ad8c16b42bc6afb6642b259d60
Aiden-FE <aiden_jin@outlook.com>
2024-10-19 15:39:06 +0800

    fix: deploy mobile error

--- apps/mobile/vite.config.ts
+++ apps/mobile/vite.config.ts
@@ -8,7 +8,7 @@ import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/palm-cloud-mobile/',
+  base: './',
   plugins: [
     uni(),
     Icons({
